6263993d231e75d25e3cbe7c1ab71259c8896b3d
HCDUNG <dungb1906640@gmail.com>
2024-10-11 01:08:21 +0700

    mod layout, dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
+@extends('layouts.breadcrumb')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="row justify-content-center">
+    <div class="col-md-12">
+        <div class="card bg-white">
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                {{ __('You are logged in!') }}
             </div>
         </div>
     </div>
