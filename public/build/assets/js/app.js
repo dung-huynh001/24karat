99b518f67da94c2ef3c20adff6a3ac282198e2a6
hcdung <dungb1906640@gmail.com>
2024-10-31 18:56:24 +0900

    mod validate

--- public/build/assets/js/app.js
+++ public/build/assets/js/app.js
@@ -1,8 +1,34 @@
 /* Add Validate Rules */
-$.validator.addMethod("hasSpecialCharacter", (value, element) => {
-    return this.optional(element) || /[!@#$%^&*\-_=+]/.test(value);
-});
+$.validator.addMethod(
+    "hasSpecialCharacter",
+    function (value, element) {
+        return this.optional(element) || /[!@#$%^&*\-_=+]/.test(value);
+    },
+    "パスワードは最低1特殊文字を含む必要があります。特殊文字 （ !@#$%^&*-_+=）"
+);
 
-$.validator.addMethod("hasDigital", (value, element) => {
-    return this.optional(element) || /\d/.test(value);
+$.validator.addMethod(
+    "hasDigital",
+    function (value, element) {
+        return this.optional(element) || /\d/.test(value);
+    },
+    "パスワードは最低1桁数を含む必要があります。"
+);
+
+$.validator.addMethod(
+    "customEmail",
+    function (value, element) {
+        // Biểu thức regex để kiểm tra định dạng email
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return this.optional(element) || emailPattern.test(value);
+    },
+    "電子メール アドレスの形式が正しくありません。"
+);
+
+/* Modify error messages */
+$.extend($.validator.messages, {
+    required: "必須フィールドに入力してください",
+    email: "電子メール アドレスの形式が正しくありません。",
+    minlength: "パスワード（確認）の文字数は、8文字以上である必要があります。",
+    equalTo: "パスワード（確認）と新しいパスワードが一致しません。",
 });
--- resources/views/manager/register.blade.php
+++ resources/views/manager/register.blade.php
@@ -2,17 +2,17 @@
 @extends('layouts.breadcrumb')
 @section('content')
 <style>
-    .pageLoader {
-        background: url(../images/loader.gif) no-repeat center center;
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-        z-index: 9999999;
-        background-color: #ffffff8c;
+.pageLoader {
+    background: url(../images/loader.gif) no-repeat center center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 9999999;
+    background-color: #ffffff8c;
 
-    }
+}
 </style>
 
 <div class="container-fluid">
@@ -25,21 +25,22 @@
                 <select name="subscription_user" class="form-select" aria-label="契約ユーザー">
                     <option value="" disabled selected>--選択--</option>
                     @foreach ($subscriptionUsers as $subscriptionUser)
-                        <option value="{{ $subscriptionUser['subscription_user_id'] }}" {{ old('subscription_user') == $subscriptionUser['subscription_user_id'] ? 'selected' : '' }}>
-                            {{ $subscriptionUser['company_name'] }}
-                        </option>
+                    <option value="{{ $subscriptionUser['subscription_user_id'] }}"
+                        {{ old('subscription_user') == $subscriptionUser['subscription_user_id'] ? 'selected' : '' }}>
+                        {{ $subscriptionUser['company_name'] }}
+                    </option>
                     @endforeach
                 </select>
                 @if ($errors->has('subscription_user'))
-                    @foreach ($errors->get('subscription_user') as $subscriptionUserErrors)
-                        @if (is_array($subscriptionUserErrors))
-                            @foreach ($subscriptionUserErrors as $msg)
-                                <div class="text-sunset-orange error-messages">{{$msg}}</div>
-                            @endforeach
-                        @else
-                            <div class="text-sunset-orange error-messages">{{$subscriptionUserErrors}}</div>
-                        @endif
-                    @endforeach
+                @foreach ($errors->get('subscription_user') as $subscriptionUserErrors)
+                @if (is_array($subscriptionUserErrors))
+                @foreach ($subscriptionUserErrors as $msg)
+                <div class="text-sunset-orange error-messages">{{$msg}}</div>
+                @endforeach
+                @else
+                <div class="text-sunset-orange error-messages">{{$subscriptionUserErrors}}</div>
+                @endif
+                @endforeach
                 @endif
             </div>
         </div>
@@ -49,7 +50,7 @@
             <div class="col-sm-10">
                 <input type="text" name="name" class="form-control" value="{{ old('name') }}" autocomplete="off">
                 @error('name')
-                    <div class="text-sunset-orange error-messages">{{ $message }}</div>
+                <div class="text-sunset-orange error-messages">{{ $message }}</div>
                 @enderror
             </div>
         </div>
@@ -57,18 +58,17 @@
         <div class="mb-3 row">
             <label for="email" class="col-sm-2 col-form-label">メールアドレス <span class="text-sunset-orange">*</span></label>
             <div class="col-sm-10">
-                <input name="email" class="form-control" placeholder="email@example.com" autocomplete="off"
-                    value="{{ old('email') }}">
+                <input name="email" class="form-control" placeholder="email@example.com" autocomplete="off">
                 @if ($errors->has('email'))
-                    @foreach ($errors->get('email') as $emailErrors)
-                        @if (is_array($emailErrors))
-                            @foreach ($emailErrors as $msg)
-                                <div class="text-sunset-orange error-messages">{{$msg}}</div>
-                            @endforeach
-                        @else
-                            <div class="text-sunset-orange error-messages">{{$emailErrors}}</div>
-                        @endif
-                    @endforeach
+                @foreach ($errors->get('email') as $emailErrors)
+                @if (is_array($emailErrors))
+                @foreach ($emailErrors as $msg)
+                <div class="text-sunset-orange error-messages">{{$msg}}</div>
+                @endforeach
+                @else
+                <div class="text-sunset-orange error-messages">{{$emailErrors}}</div>
+                @endif
+                @endforeach
                 @endif
             </div>
         </div>
@@ -79,15 +79,15 @@
             <div class="col-sm-10">
                 <input type="password" name="password" class="form-control" autocomplete="off">
                 @if ($errors->has('password'))
-                    @foreach ($errors->get('password') as $passwordErrors)
-                        @if (is_array($passwordErrors))
-                            @foreach ($passwordErrors as $msg)
-                                <div class="text-sunset-orange error-messages">{{$msg}}</div>
-                            @endforeach
-                        @else
-                            <div class="text-sunset-orange error-messages">{{$passwordErrors}}</div>
-                        @endif
-                    @endforeach
+                @foreach ($errors->get('password') as $passwordErrors)
+                @if (is_array($passwordErrors))
+                @foreach ($passwordErrors as $msg)
+                <div class="text-sunset-orange error-messages">{{$msg}}</div>
+                @endforeach
+                @else
+                <div class="text-sunset-orange error-messages">{{$passwordErrors}}</div>
+                @endif
+                @endforeach
                 @endif
             </div>
         </div>
@@ -98,15 +98,15 @@
             <div class="col-sm-10">
                 <input type="password" name="confirm_password" class="form-control" autocomplete="off">
                 @if ($errors->has('confirm_password'))
-                    @foreach ($errors->get('confirm_password') as $confirmPasswordErrors)
-                        @if (is_array($confirmPasswordErrors))
-                            @foreach ($confirmPasswordErrors as $msg)
-                                <div class="text-sunset-orange error-messages">{{$msg}}</div>
-                            @endforeach
-                        @else
-                            <div class="text-sunset-orange error-messages">{{$confirmPasswordErrors}}</div>
-                        @endif
-                    @endforeach
+                @foreach ($errors->get('confirm_password') as $confirmPasswordErrors)
+                @if (is_array($confirmPasswordErrors))
+                @foreach ($confirmPasswordErrors as $msg)
+                <div class="text-sunset-orange error-messages">{{$msg}}</div>
+                @endforeach
+                @else
+                <div class="text-sunset-orange error-messages">{{$confirmPasswordErrors}}</div>
+                @endif
+                @endforeach
                 @endif
             </div>
         </div>
@@ -129,42 +129,55 @@
 </div>
 
 <script>
-    $('#managerForm').on('submit', (event) => {
-        event.preventDefault();
-
+$(document).ready(function() {
+    $('#managerForm').on('submit', function(event) {
+        validateForm();
+        if ($(this).valid()) {
+            return true;
+        } else {
+            event.preventDefault();
+        }
     });
+});
 
-    function validateForm() {
-        $('#managerForm').validate({
-            rules: {
-                "subscription_user": {
-                    required: true;
-                },
-                "name": {
-                    required: true,
-                },
-                "email": {
-                    required: true,
-                    email: true,
-                },
-                "password": {
-                    required: true,
-                    minlength: 8,
-                    hasDigital: true,
-                    hasSpecialCharacter: true,
-                },
-                "confirm_password": {
-                    required: true,
-                    minlength: 8,
-                    equalTo: "password"
-                },
+function validateForm() {
+    $('#managerForm').validate({
+        rules: {
+            subscription_user: {
+                required: true
             },
-            messages: {
-
+            name: {
+                required: true
             },
-
-        });
-    }
+            email: {
+                required: true,
+                customEmail: true
+            },
+            password: {
+                required: true,
+                minlength: 8,
+                hasDigital: true,
+                hasSpecialCharacter: true,
+            },
+            confirm_password: {
+                required: true,
+                minlength: 8,
+                equalTo: "[name='password']"
+            }
+        },
+        errorClass: "text-sunset-orange",
+        errorPlacement: function(error, element) {
+            let $errorContainer = $(element).siblings(".error-container");
+            
+            if ($errorContainer.length === 0) {
+                $errorContainer = $("<div class='error-container'></div>").insertAfter(element);
+            }
+            
+            $errorContainer.append(error);
+        },
+    });
+}
 </script>
+
 
 @endsection
